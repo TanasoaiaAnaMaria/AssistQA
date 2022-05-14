c8af76d23f34eb7f7235e07269ab73a8d310960b
Ana-Maria Tănăsoaia <tanasoaiaanamaria@gmail.com>
2022-05-14 14:11:15 +0300

    T01 case

---
+++ cypress/integration/QAM/T01_openPage.spec.js
@@ -0,0 +1,8 @@
+describe("Open", function(){
+    it("open",function() {
+        cy.visit('https://assist-devqa.netlify.app')
+        cy.title().should('eq','BIM')
+        cy.location('protocol').should('eg','https')
+        
+    })
+})
