53b240924ba89b9d1358775066877e4263d8cb56
Ana-Maria Tănăsoaia <tanasoaiaanamaria@gmail.com>
2022-05-14 19:16:31 +0300

    login test and login function

--- cypress/integration/QAM/T03_login.spec.js
+++ cypress/integration/QAM/T03_login.spec.js
@@ -1,6 +1,14 @@
 describe("Login" , function(){
+    
+
     it('login',function(){
+
         cy.visit('https://assist-devqa.netlify.app')
+
+        
+        cy.get('.logged-menu').should('be.visible').click()
+        cy.get('.dropdown-child').contains('Log out').click()
+
         cy.get('.btn-nav').contains('Log In').click()
         cy.get('.btn-login').contains('Login with your existing account').click()
         cy.get('input[name="email"]').type('user10@test.com')
